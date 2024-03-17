96132143c992b5e23ad68804da99f8f3c02b2936
Mihai <mihaipaulx@gmail.com>
2024-03-17 11:50:35 +0200

    basic structure

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "title": "Blog post 1",
+        "author": "Mihai",
+        "content": "This is the first blog post",
+        "date_posted": "March 16, 2024"
+    },
+    {
+        "title": "Blog post 2",
+        "author": "Bob",
+        "content": "This is the second blog post",
+        "date_posted": "March 15, 2024"
+    },
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About page</h1>"
+    return render_template("about.html", title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
