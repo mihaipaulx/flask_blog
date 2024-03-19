59ddb726e304d4d166fccfd222d27caa077aa0ab
Mihai <mihaipaulx@gmail.com>
2024-03-20 00:37:58 +0200

    pass reset

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -14,18 +14,16 @@ class RegistrationForm(FlaskForm):
   submit = SubmitField("Sign Up")
 
   def validate_username(self, username):
-    if not current_user.is_authenticated or username.data != current_user.username:
-      user = User.query.filter_by(username=username.data).first()
+    user = User.query.filter_by(username=username.data).first()
 
-      if user:
-        raise ValidationError("That username is taken. Please choose a different one.")
+    if user:
+      raise ValidationError("That username is taken. Please choose a different one.")
     
   def validate_email(self, email):
-    if not current_user.is_authenticated or email.data != current_user.email:
-      user = User.query.filter_by(email=email.data).first()
+    user = User.query.filter_by(email=email.data).first()
 
-      if user:
-        raise ValidationError("That email is taken. Please choose a different one.")
+    if user:
+      raise ValidationError("That email is taken. Please choose a different one.")
 
 class LoginForm(FlaskForm):
   email = StringField("Email", validators=[DataRequired(), Email()])
@@ -60,3 +58,21 @@ class PostForm(FlaskForm):
   content = TextAreaField("Content", validators=[DataRequired()])
 
   submit = SubmitField("Post")
+
+class RequestResetForm(FlaskForm):
+  email = StringField("Email", validators=[DataRequired(), Email()])
+
+  submit = SubmitField("Request Password Reset")
+
+  def validate_email(self, email):
+    user = User.query.filter_by(email=email.data).first()
+
+    if user is None:
+        raise ValidationError("There is no account with that email. You must register first.")
+    
+class ResetPasswordForm(FlaskForm):
+  password = PasswordField("Password", validators=[DataRequired()])
+  confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+  
+  submit = SubmitField("Reset Password")
+
